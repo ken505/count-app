1b5dcede53086ce4531906fd13df802594658294
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-05 16:55:07 +0900

    🐛 Fix a bug.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { VFC } from "react";
+// import { VFC } from "react";
 import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
@@ -9,7 +9,11 @@ import "../styles/globals.css";
 
 // 👆👇どっちがいいのだろう？🤔 両方とも型定義できていて暗黙の Children は渡せない感じがするけれど・・。しかも👇に VFC は追記可能でエラーもでない。よくわからん。
 
-const MyApp: VFC = (props: AppProps) => {
-  return <props.Component {...props.pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
 };
+
+// const MyApp = (props: AppProps) => {
+//   return <props.Component {...props.pageProps} />;
+// };
 export default MyApp;
